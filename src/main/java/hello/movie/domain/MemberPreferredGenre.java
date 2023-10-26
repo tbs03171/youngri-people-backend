c94b2c6ffd314573bccd0868ea363c33471ce3d2
tbs03171 <tbs03171@naver.com>
2023-10-26 16:21:53 +0900

    feat: MemberPreferredGenre 구현

---
+++ src/main/java/hello/movie/domain/MemberPreferredGenre.java
@@ -0,0 +1,23 @@
+package hello.movie.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+
+@Entity
+@Getter
+@Builder
+public class MemberPreferredGenre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_PREFERRED_GENRE_ID")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Enumerated(EnumType.STRING)
+    private Genre genre;
+}
