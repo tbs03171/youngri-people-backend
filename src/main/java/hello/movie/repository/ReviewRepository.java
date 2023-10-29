a044893435873f97f3b85c08cb9f1f881f601e5a
Nctaul <dohyeonggim832@gmail.com>
2023-10-29 23:26:41 +0900

    Revert "review 구현"
    This reverts commit f5dbec88bec42ded46f5b217bbb621d31bccdff6.

--- src/main/java/hello/movie/repository/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package hello.movie.repository;
-
-import hello.movie.domain.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review,Long> {
-}
