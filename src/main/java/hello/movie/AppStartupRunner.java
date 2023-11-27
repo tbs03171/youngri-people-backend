2c64d32ad261b1d954a58dce219039335be560c0
tbs03171 <tbs03171@naver.com>
2023-11-27 15:48:20 +0900

    feat: 영화 데이터 초기 적재 기능 추가
    - AppStartupRunner 클래스에 initializeDefaultData 매서드 구현
    - 프로젝트 실행 시 초기 데이터 데이터베이스에 삽입

--- src/main/java/hello/movie/AppStartupRunner.java
+++ src/main/java/hello/movie/AppStartupRunner.java
@@ -1,6 +1,5 @@
 package hello.movie;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import hello.movie.dto.MemberDto.CreateMemberDto;
 import hello.movie.service.MemberService;
 import hello.movie.service.MovieService;
@@ -17,29 +16,30 @@ public class AppStartupRunner implements CommandLineRunner {
     private final MovieService movieService;
 
     @Override
-    public void run(String... args) throws Exception {
-        createTestMember();
+    public void run(String... args) {
+        initializeDefaultData();
     }
 
     /**
-     * 기본 계정 생성
+     * 초기 데이터 설정
      */
-    private void createTestMember() throws JsonProcessingException {
-        // 계정 하나 생성
+    private void initializeDefaultData()  {
+        // 계정 생성
         CreateMemberDto createMemberDto1 = new CreateMemberDto();
         createMemberDto1.setUserId("test");
         createMemberDto1.setPassword("0000");
 
-        // 계정 하나 생성
         CreateMemberDto createMemberDto2 = new CreateMemberDto();
         createMemberDto2.setUserId("test2");
         createMemberDto2.setPassword("0000");
 
-        // 영화 데이터
-        movieService.getMovieById(496243L);
-        movieService.getMovieById(372058L);
-
         memberService.join(createMemberDto1);
         memberService.join(createMemberDto2);
+
+        // 영화 데이터
+        movieService.getPopularMovies();
+        movieService.getUpcomingMovies();
+        movieService.getTopRatedMovies();
+        movieService.getNowPlayingMovies();
     }
 }
