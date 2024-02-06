faf498b2aa5c44e5a883411404414ac1d6ae2ff3
tbs03171 <tbs03171@naver.com>
2024-02-06 13:09:41 +0900

    feat: EnableCaching 적용

--- src/main/java/hello/movie/MovieApplication.java
+++ src/main/java/hello/movie/MovieApplication.java
@@ -2,13 +2,16 @@ package hello.movie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class MovieApplication {
 
