9553f5ad64988ffa342407eb12ceb95fd4710890
tbs03171 <tbs03171@naver.com>
2024-02-10 14:19:52 +0900

    style: 필요 없는 코드 제거

--- src/main/java/hello/movie/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/hello/movie/config/CustomAuthenticationFailureHandler.java
@@ -1,15 +1,9 @@
 package hello.movie.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import hello.movie.CustomResponse;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
